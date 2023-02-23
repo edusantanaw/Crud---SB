c6cb59dd3dd59b3ca28611bc61368ee8ca374a9e
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-02-23 17:25:27 -0300

    fix: userrepository types

--- src/main/java/com/edusantanaw/crud/infra/UserRepository.java
+++ src/main/java/com/edusantanaw/crud/infra/UserRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<UserDetails> findByEmail(String email);
 }
