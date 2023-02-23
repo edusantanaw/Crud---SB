14da5a3ac287e18f3748ae9d36df3bfa115f3870
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-02-23 16:53:13 -0300

    feat: Application config

--- src/main/java/com/edusantanaw/crud/infra/UserRepository.java
+++ src/main/java/com/edusantanaw/crud/infra/UserRepository.java
@@ -2,7 +2,10 @@ package com.edusantanaw.crud.infra;
 
 import com.edusantanaw.crud.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository {
-    User  findByEmail(String email);
+    Optional<UserDetails> findByEmail(String email);
 }
